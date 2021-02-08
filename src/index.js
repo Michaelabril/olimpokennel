f83d20ce321aad9f96cb7433662b4dd5cff9f93c
michaelabril <maiicolabriil@hotmail.com>
2021-02-08 10:10:36 -0500

    prueba

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+//entiende los datos del formulario
+app.use(express.urlencoded({extended: false})); 
+app.use(express.json());
+
+//settings
+app.set('port', process.env.PORT || 8081);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+// middlewares
+
+//routes
+app.use(require('./routes/index.js'));
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//listening server
+app.listen(app.get('port'), () => {
+    console.log('server on port', app.get('port'));
+});
+
---
+++ src/routes/index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router = express.Router();
+const nodemailer = require('nodemailer')
+//route view page path
+router.get('/', (req, res) => {
+    res.render('index.html', { title: 'Olimpo kennel' });
+});
+
+//route view contact page
+router.get('/contact', (req, res) => {
+    res.render('contact.html');
+});
+
+//route view services page
+router.get('/services', (req, res) => {
+    res.render('services.html');
+});
+
+router.get('/proyectos', (req, res) => {
+    res.render('proyectos.html');
+});
+
+router.get('/machos', (req, res) => {
+    res.render('machos.html');
+});
+
+router.get('/hembras', (req, res) => {
+    res.render('hembras.html');
+});
+
+router.get('/camadas', (req, res) => {
+    res.render('camadas.html');
+});
+
+router.get('/about', (req, res) => {
+    res.render('about.html');
+});
+router.post('/send-email', async (req, res) => {
+    const {name, email, phone, message} = req.body;        
+    contentHTML = `
+        <h1>Detalle del usuario</h1>
+        <ul>
+            <li>Username: ${name}</li>
+            <li>User Email: ${email}</li>
+            <li>PhoneNumber: ${phone}</li>
+        </ul>
+        <p>${message}</p>
+    `;
+    const transporter= nodemailer.createTransport({
+        host:'mail.constructorama.x10.mx', //host de correo a donde se va enviar 
+        port:25,
+        secure: false,
+        auth:{
+            user:'testemail@constructorama.x10.mx',//usuario donde se recibe el correo
+            pass:'123456mc' //
+        },
+        tls:{
+            rejectUnauthorized: false
+        }
+    });
+
+   const info = await transporter.sendMail({
+        from: '"servidor de correos" <testemail@constructorama.x10.mx>', // host de correo 
+        to: 'michaelalejandroabrilmarmolejo@gmail.com',
+        subject: 'servicio',
+        html: contentHTML
+    });
+
+    console.log("mensaje enviado", info.messajeId);
+    res.redirect('/');
+});
+module.exports = router;
